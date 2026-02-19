1803dd583f5646ef8f32c80496ed8d776b8fb38e
Mohit M <mohitmajhi99@gmail.com>
2026-02-19 20:44:26 +0000

    added pod name return in flask app

--- flask_app/main.py
+++ flask_app/main.py
@@ -1,10 +1,16 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello from Alice 👋"
+
+@app.route("/pod")
+def pod():
+    pod_name = os.environ.get("POD_NAME") or os.uname().nodename
+    return f"Hello from pod: {pod_name}\n"
 
 @app.route("/health")
 def health():
